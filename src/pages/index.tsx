c0b2e8b1771b4e58cba3b0e72df1681bbd62e1d4
Josiah <griggs@DN0a1f294d.SUNet>
2023-05-23 12:41:01 -0700

    Updated to accept any input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [input, setInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event: { preventDefault: () => void }) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ input: input }),
       });
 
       const data = await response.json();
@@ -26,9 +26,8 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setInput("");
     } catch (error) {
-      // Consider implementing your own error handling logic here
       const e = error as Error;
       console.error(e);
       alert(e.message);
@@ -44,20 +43,20 @@ export default function Home() {
 
       <main className="flex flex-col items-center pt-60">
         <img src="/dog.png" className={styles.icon} />
-        <h3 className="m-16 text-3xl text-black">Name my pet</h3>
+        <h3 className="m-16 text-3xl text-black">Trade Plan Generator</h3>
         <form onSubmit={onSubmit} className="flex w-[320px] flex-col">
           <input
             className="mb-4 border border-gray-300 p-2"
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="input"
+            placeholder="Enter your input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
           <input
             className="cursor-pointer rounded bg-blue-600 px-12 py-4 text-center text-white"
             type="submit"
-            value="Generate names"
+            value="Generate"
           />
         </form>
         <div className="mt-40 border p-12 font-bold">{result}</div>
